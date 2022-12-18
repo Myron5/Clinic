48bf62b5001b5bd5eccab86e4c4d46594de25542
unknown <myron.melnyk.kn.2021@lpnu.ua>
2022-12-18 15:22:05 +0200

    debugSlider

--- JS/abbility-slider.js
+++ JS/abbility-slider.js
@@ -3,31 +3,32 @@ const el = document.querySelector('.abbility__list');
 if (window.screen.width < 1290) {
   el.classList.add('SLIDER');
 
-  if (window.screen.width >= 720) {
-    $('.SLIDER').slick({
-      arrows: false,
-      dots: true,
-      infinite: false,
-      slidesToShow: 2,
-    });
-  } else {
-    $('.SLIDER').slick({
-      arrows: false,
-      dots: true,
-      infinite: false,
-      slidesToShow: 1,
-    });
-  }
+  $('.SLIDER').slick({
+    arrows: false,
+    dots: true,
+    infinite: false,
+  });
+
+  if (window.screen.width >= 720) $('.SLIDER').slick('slickSetOption', 'slidesToShow', 2);
+  else $('.SLIDER').slick('slickSetOption', 'slidesToShow', 1);
 }
 
 window.addEventListener('resize', e => {
-  if (window.screen.width >= 1290) el.classList.remove('SLIDER');
-
-  if (window.screen.width < 1290) {
-    if (window.screen.width >= 720) {
-      $('.SLIDER').slick('slickSetOption', 'slidesToShow', 2);
-    } else {
-      $('.SLIDER').slick('slickSetOption', 'slidesToShow', 1);
+  if (window.screen.width >= 1290) {
+    if (el.classList.contains('slick-initialized')) {
+      el.classList.remove('SLIDER');
+      el.slick.unslick();
     }
+  } else {
+    if (!el.classList.contains('slick-initialized')) {
+      el.classList.add('SLIDER');
+      $('.SLIDER').slick({
+        arrows: false,
+        dots: true,
+        infinite: false,
+      });
+    }
+    if (window.screen.width >= 720) $('.SLIDER').slick('slickSetOption', 'slidesToShow', 2);
+    else $('.SLIDER').slick('slickSetOption', 'slidesToShow', 1);
   }
 });
